a1abee69e957bfcfb568a0aaf0b77d70c95313ba
bwatson78 <brad.watson.orlando@gmail.com>
2019-08-05 11:32:01 -0500

    starts board

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,11 @@
 class Board
+	attr_accessor :spaces
 
+	def clean_board
+		@spaces = Array.new(9, "")
+	end
+
+	def initialize
+		clean_board
+	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,4 +3,11 @@ require "spec_helper"
 
 describe "Board" do
 
+	context "#initialize" do
+		it "creates an instance of a board" do
+			board = Board.new
+			expect(board.spaces).to eq(["", "", "", "", "", "", "", "", ""])
+		end
+	end
+
 end 
